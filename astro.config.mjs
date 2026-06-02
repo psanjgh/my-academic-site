51034d57bce3fe58fa6fda4024da2172a9c28b1a
PeiYu Cai <cpyexp@gmail.com>
2026-06-02 16:49:47 +0100

    Modified Readme and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://rubzip.github.io',
-  base: '/academic-portfolio-astro',
+  site: 'https://psanjgh.github.io',
+  base: '/my-academic-site',
   integrations: [sitemap()],
 });
